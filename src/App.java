1050aa4ab8698f7eca7ca6ee95e5c108cd40a970
LucasVz <lucastadeuvaz@gmail.com>
2023-01-10 14:49:59 -0300

    add: multiplica ai

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 6;
+        System.out.println("Tabela de multiplicação de " + numero);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " * " + i + " = " + numero * i);
+        }
+    }
+}
